8f14948ca734b80bd08e942c0a6577110dea252f
symar-207 <omelonganathan7@gmail.com>
2026-01-08 04:20:20 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-document.querySelector("form").addEventListener("submit", function(e) {
-  e.preventDefault();
+function enregistrerUtilisateur(event) {
+  event.preventDefault();
 
   const nom = document.getElementById("nom").value;
   const email = document.getElementById("email").value;
@@ -10,9 +10,23 @@ document.querySelector("form").addEventListener("submit", function(e) {
     email: email,
     motdepasse: motdepasse
   };
-
   localStorage.setItem("utilisateur", JSON.stringify(user));
-alert("formulaire soumis!")
-  // Redirection vers ton site de formation
-  window.location.href = "https://www.youtube.com/user/KhanAcademyFrancais "; // remplace par le lien réel
-});
+
+// Vide le contenu de la page
+document.getElementById("contenu").innerHTML = "";
+
+// Affiche une alerte
+
+// Message personnalisé avec le nom
+document.getElementById("contenu").innerHTML =
+  "<p>Chargement des vidéos pour <strong>" + user.nom + "</strong>...</p>";
+
+// Redirection après 2 secondes
+setTimeout(() => {
+  window.location.href = "https://www.youtube.com/user/KhanAcademyFrancais";
+}, 3000);} 
+
+ 
+
+
+
